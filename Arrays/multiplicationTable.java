23f141dc12f76f3694fff63a00f58b2f39fb08f4
mehtashish <mehta.mtj@gmail.com>
2025-06-16 09:58:51 +0530

    [Ashish] Fix. File name fixed as per java naming convention

--- Arrays/multiplicationTable.java
+++ Arrays/multiplicationTable.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class multiplicationTable {
+public class MultiplicationTable {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
