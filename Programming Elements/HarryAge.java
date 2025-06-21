2ef9cc2b83d424d7c70e06e4e7908813ac3a21fe
mehtashish <mehta.mtj@gmail.com>
2025-06-21 17:42:59 +0530

    [Ashish] Add. Program to calculate Harry's age

---
+++ Programming Elements/HarryAge.java
@@ -0,0 +1,13 @@
+public class HarryAge {
+    public static void main(String[] args) {
+        // Birth year of Harry
+        int birthYear = 2000;
+
+        // Assuming current year to be 2024
+        int currentYear = 2024;
+
+        // Calculating and displaying current age of Harry
+        int age = currentYear - birthYear;
+        System.out.println("Harry's age in 2024 is " + age);
+    }
+}
